aca645a7ad57e9a74ac67805c440de15db3cc2e5
Jim Brannon <jim@brannondevelopments.com>
2014-09-17 23:28:28 -0600

    add category_range and category_period args to wrapper code category_range applies to number type category index fields (only) category_period applies to date type category index fields (only) category_period units are days code adjusts the min max category values as necessary

--- pg2gv_wrapper.php
+++ pg2gv_wrapper.php
@@ -31,6 +31,8 @@ $category_label_field = ""; // a string = "" is the official default
 $category_show_all_arg = "false"; // a string = "false" is the official default
 $category_min = ""; // a string = "" is the official default
 $category_max = ""; // a string = "" is the official default
+$category_range = ""; // a string = "" is the official default
+$category_period = ""; // a string = "" is the official default
 $series_table_name = ""; // a string = "" is the official default
 $series_fields = ""; // a string = "" is the official default -  comma delimited, NO SPACES!!
 $series_index_field = ""; // a string = "" is the official default
@@ -46,6 +48,119 @@ $category_axis_label = ""; // a string = "" is the official default
 $series_axis_label = ""; // a string = "" is the official default
 $conv_factor = 1.0; // a real = 1.0 is the official default
 $precision = 99; // an integer = 99 is the official default
+/*
+ * code specific to the category min/max filters
+ * ---------------------------------------------
+ * the below code (as it originally exists) in the lre git repo
+ * is generic enough that it can be left in the wrapper without
+ * affecting anything else
+ * (but if you change it, caveat emptor)
+ * the idea is this:
+ * add a category index "period"  "range" url arg handling routine to THIS wrapper
+ * then if this arg is defined, adjust the default category min max filter values accordingly
+ * several potential cases:
+ * "range" not defined => do nothing and the above min max defaults will be used and
+ *                         the DMI will over-ride them with the URL arg min max values
+ *                         if they exist (this is the default behavior)
+ * "range defined" =>
+ * 		1. use the min max defaults above, and over-ride them with
+ *		the category min max URL args, might need these in step 2
+ *		2. adjust the MISSING min max defaults as follows:
+ *			if BOTH have values => do nothing, "range" value will be ignored and DMI will use min max
+ *			if NEITHER is defined =>
+ *				A. assume category index field is a number type (integer, long, real, single, double) (anything else and it probably breaks!!!)
+ *				B. min = 0
+ *				C. max = min + range
+ * 			if MIN is defined (and not max) =>
+ * 				A. max = min + range
+ * 			if MAX is defined (and not min) =>
+ * 				A. min = max - range
+ * this should be all the cases, and in all of them both min and max will
+ * 	end up with the desired value in the DMI
+ * 
+ * "period" not defined => do nothing and the above min max defaults will be used and
+ *                         the DMI will over-ride them with the URL arg min max values
+ *                         if they exist (this is the default behavior)
+ * "period defined" =>
+ * 		1. use the min max defaults above, and over-ride them with
+ *		the category min max URL args, might need these in step 2
+ *		2. adjust the MISSING min max defaults as follows:
+ *			if BOTH have values => do nothing, "period" value will be ignored and DMI will use min max
+ *			if NEITHER is defined =>
+ *				A. assume category index field is type date (anything else and it breaks!!!)
+ *				   and category period units are days
+ *				B. max = now()
+ *				C. min = max - period
+ * 			if MIN is defined (and not max) =>
+ * 				A. max = min + period
+ * 			if MAX is defined (and not min) =>
+ * 				A. min = max - period
+ * this should be all the cases, and in all of them both min and max will
+ * 	end up with the desired value in the DMI
+ * 
+ */
+$category_min = getargs ("category_min",$category_min);
+$category_max = getargs ("category_max",$category_max);
+$category_range = getargs ("category_range",$category_range);
+$category_period = getargs ("category_period",$category_period);
+if (strlen(trim($category_range))) {
+	if (strlen(trim($category_min))) {
+		if (strlen(trim($category_max))) {
+			// both defined, do nothing
+		} else {
+			// min defined, calculate max
+			// convert to value, adjust, convert to string
+			$rangeval = $category_range + 0;
+			$minval = $category_min + 0;
+			$maxval = $minval + $rangeval;
+			$category_max = strval($maxval);
+		}
+	} elseif (strlen(trim($category_max))) {
+		// max defined, calculate min
+			// convert to value, adjust, convert to string
+			$rangeval = $category_range + 0;
+			$maxval = $category_max + 0;
+			$minval = $maxval - $rangeval;
+			$category_min = strval($minval);
+	} else {
+		// neither defined
+		$rangeval = $category_range + 0;
+		$minval = 0;
+		$category_min = strval($minval);
+		$maxval = $minval + $rangeval;
+		$category_max = strval($maxval);
+	}
+} elseif (strlen(trim($category_period))) {
+	if (strlen(trim($category_min))) {
+		if (strlen(trim($category_max))) {
+			// both defined, do nothing
+		} else {
+			// min defined, calculate max
+			// convert to value, adjust, convert to string
+			$minval = DateTime::createFromFormat('Y-m-d', $category_min);
+			$periodval = $category_period + 0;
+			$maxval = $minval->modify("+$periodval day");
+			$category_max = $maxval->format('Y-m-d');
+		}
+	} elseif (strlen(trim($category_max))) {
+		// max defined, calculate min
+		// convert to value, adjust, convert to string
+		// try this without casting it, see if it works...
+		$maxval = DateTime::createFromFormat('Y-m-d', $category_max);
+		$periodval = $category_period + 0;
+		$minval = $maxval->modify("-$periodval day");
+		$category_min = $minval->format('Y-m-d');
+	} else {
+		// neither defined
+		$maxval = new DateTime();
+		$category_max = $maxval->format('Y-m-d');
+		$periodval = $category_period + 0;
+		$minval = $maxval->modify("-$periodval day");
+		$category_min = $minval->format('Y-m-d');
+	}
+} else {
+	// period or range not defined, so do nothing
+}
 /*
  * call the pg_to_gviz_basic function
  * 
